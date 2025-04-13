0d4cd57511d3ea11993ac0d12b3e4cbedc5ef23e
SohamNandi06 <soham943325@gmail.com>
2025-04-13 19:39:25 +0530

    Changed App.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import TodoApp from './TodoApp';
 import Counter from './Counter';
 import NameForm from './NameForm';
+import './App.css';
 
 function App() {
   const [activeComponent, setActiveComponent] = useState(null); // no component shown at start
@@ -22,9 +23,9 @@ function App() {
   return (
     <div>
       <div style={{ marginBottom: '1rem' }}>
-        <button onClick={() => setActiveComponent('counter')}>Show Counter</button>
-        <button onClick={() => setActiveComponent('form')}>Show Name Form</button>
-        <button onClick={() => setActiveComponent('todo')}>Show Todo App</button>
+        <button className='counter-btn' onClick={() => setActiveComponent('counter')}>Show Counter</button>
+        <button className='form-btn' onClick={() => setActiveComponent('form')}>Show Name Form</button>
+        <button className='todo-btn' onClick={() => setActiveComponent('todo')}>Show Todo App</button>
       </div>
       {renderComponent()}
     </div>
